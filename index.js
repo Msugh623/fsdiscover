fe7c0d8a6b2b4d9de5f359a2e1eacf82f2d56f3d
Ernest Chia <iternenge469@gmail.com>
2025-03-11 07:26:03 +0100

    Added fs discover, prevented express.static from serving index.html

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const NetworkProbe = require('./utils/networkProbe')
 const handlers = require('./utils/handlers')
+const os = require('os')
+const path = require('path')
+const dirname = require('./dirname')
 
 const app = express()
 const port = 3000
 const netProb =new NetworkProbe()
 const netFace = netProb.autoDetect()
+
+app.use(express.static(path.join(dirname(), 'public')))
+
+app.use(express.static(os.homedir(), {
+    index: false
+}))
 
 app.head('*', handlers.header)
 app.get('*', handlers.getPath)
--- utils/handlers.js
+++ utils/handlers.js
@@ -1,10 +1,67 @@
+// const dirname = require("../dirname")
+const path = require('path')
+const os = require('os')
+const { exec } = require("child_process")
+// const { createReadStream } = require("fs")
+// const { outputfile, tempdir } = require("../variables")
+const render = require("./render")
+
+const {homedir,platform}=os
+
 class Handlers {
     header = (_, res) => {
         res.send('Heartbeat Live')
     }
-    getPath=(req, res) => {
-        console.log('Site Visited @ path:'+req.url)
-        res.send('Hello World!')
+    getPath =  (req, res) => {
+        try {
+            const pathname = req.url
+            this[('fs' + platform())](pathname,async (data) => {
+                // console.log(data.split('\n'))
+                res.set('Content-Type', 'text/html')
+                const prsData = await render('Sprint FS Explorer - index of: ' + pathname, data)
+                res.send(prsData)
+            })
+        } catch (error) {
+            // console.error(error)
+            res.status(500).send(error)
+        }
+    }
+    fswin32 = (pathname,useData) => {
+        // const outputFilePath = path.join(dirname(), tempdir,  outputfile)
+        exec(`dir /B ${path.join(homedir(), pathname)}`, (error, stdout, stderr) => {
+            if (error) {
+                useData('')
+                console.error(`exec error: ${error}`)
+                return
+            }
+            if (stderr) {
+                useData('')
+                console.error(`exec error: ${stderr}`)
+                return
+            }
+            if (stderr) {
+                throw stderr
+            }
+            
+            return useData(stdout)
+        })
+    }
+    fslinux = (pathname,useData) => {
+        // const outputFilePath = path.join(dirname(), tempdir, 'paths.txt')
+        exec(`ls ${path.join(homedir(), pathname)} `, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`)
+                useData('')
+                return
+            }
+            if (stderr) {
+                useData('')
+                console.error(`exec error: ${stderr}`)
+                return
+            }
+            
+            return useData(stdout)
+        })
     }
 }
 
